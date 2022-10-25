cb309431236d902ba86e5e64a4430bff8bdae492
Reggie P <reginaldpitts1@gmail.com>
2022-10-24 19:10:22 -0500

    Updated questions js file

---
+++ assets/js/questions.js
@@ -0,0 +1,40 @@
+// Here are the questions for my code quiz
+var questions = [ // store everything inside of a variable
+    {
+      title: "Which dragon belongs to Prince Daemon Targaryen?",
+      multiChoice: ["Syrax", "Caraxes", "Meleys", "Arrax"],
+      answer: "Caraxes"
+    },
+  
+    {
+      title: "Who did King Viserys name as his heir to the iron throne?",
+      multiChoice: ["Aemond Targaryen", "Corlys Velaryon", "Otto Hightower", "Rhaenyra Targaryen"],
+      answer: "Rhaenyra Targaryen"
+    },
+  
+    {
+      title: "Who is the father of Lucerys, Jacaerys, and Joffrey Velaryon?",
+      multiChoice: ["Daemon Targaryen", "Vaemond Velaryon", "Ser Harwin Strong", "Larys Strong"],
+      answer: "Ser Harwin Strong"
+    },
+  
+    {
+      title: "What is the name of the targaryen civil war called?",
+      multiChoice: ["Game of Thrones", "Storm of Swords", "The Dance of Dragons", "The long night"],
+      answer: "The Dance of Dragons"
+    },
+  
+    {
+      title: "Who was Rhaenyra's best friend growing up?",
+      multiChoice: ["Rhaenys Targaryen", "Alicent Hightower", "Leana Velaryon", "Daenerys Targaryen"],
+      answer: "Alicent Hightower"
+    },
+
+    {
+        title: "Where is the home to House Velaryon?",
+        multiChoice: ["Dorne", "Driftmark", "The Stormlands", "River Run"],
+        answer: "Driftmark"
+    }
+  ];
+
+  // 6 questions were made with 4 choices each
